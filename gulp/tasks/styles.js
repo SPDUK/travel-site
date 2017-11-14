430f4a3dd0460d0da80ba63323e4f52e7d31b370
SPD <spdevuk@gmail.com>
2017-11-14 05:39:31 +0000

    fixed css inject

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp'),
-postcss = require ('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssimport = require('postcss-import'),
-mixins = require('postcss-mixins');
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested'),
+  cssimport = require('postcss-import'),
+  mixins = require('postcss-mixins');
 
 
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
